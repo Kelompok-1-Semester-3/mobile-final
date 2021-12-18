c0ae30c8a62346647776d32976afbc4a8a14b128
Rifqi Febri Vandhika <gtgrifqi@gmail.com>
2021-12-18 09:16:00 +0700

    fragment_user

--- FriendFinderApp/app/src/main/java/com/example/friendfinderapp/Activity/SignIn.java
+++ FriendFinderApp/app/src/main/java/com/example/friendfinderapp/Activity/SignIn.java
@@ -1,8 +1,6 @@
 package com.example.friendfinderapp.Activity;
 
-import android.content.Context;
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
@@ -25,11 +23,9 @@ import retrofit2.Callback;
 import retrofit2.Response;
 
 public class SignIn extends AppCompatActivity {
-
     private EditText etEmail, etPassword;
     String email, password;
-    private SharedPreferences sharedPreferences;
-    public static final String MyPref = "login";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,9 +34,7 @@ public class SignIn extends AppCompatActivity {
 
         etEmail = findViewById(R.id.signinEmail);
         etPassword = findViewById(R.id.signinPassword);
-
         Button btnLogin = findViewById(R.id.btn_sign_in);
-        sharedPreferences = getSharedPreferences(MyPref, Context.MODE_PRIVATE);
 
         btnLogin.setOnClickListener(v -> {
             email = etEmail.getText().toString();
@@ -50,30 +44,16 @@ public class SignIn extends AppCompatActivity {
             } else if (password.trim().length() == 0) {
                 etPassword.setError("field tidak boleh kosong");
             } else {
-                SharedPreferences.Editor editor = sharedPreferences.edit();
-                editor.putString("email", email);
-                editor.putString("password" , password);
-                editor.commit();
                 login(email, password);
             }
         });
-
-        String prefEmail = sharedPreferences.getString("email", "");
-        String prefPassword = sharedPreferences.getString("password", "");
-        System.out.println(prefEmail + " : " + prefPassword);
-        if (prefEmail.length() > 0 && prefPassword.length() > 0) {
-            login(prefEmail, prefPassword);
-            finish();
-        }
 
         TextView link_sign_up = findViewById(R.id.link_sign_up);
         link_sign_up.setOnClickListener(v -> {
             Intent intent = new Intent(getApplicationContext(), SignUp.class);
             startActivity(intent);
         });
-
     }
-
 
     private void login(String email, String password) {
         APIRequestData apiData = RetroServer.konekRetro().create(APIRequestData.class);
@@ -86,8 +66,8 @@ public class SignIn extends AppCompatActivity {
 
                 if (status.equals("1")) {
                     Toast.makeText(getApplicationContext(), "" + pesan, Toast.LENGTH_SHORT).show();
-                    getByEmail(email);
-                } else {
+                    getByEmail();
+                }else {
                     Toast.makeText(getApplicationContext(), "Login Gagal ", Toast.LENGTH_SHORT).show();
                 }
                 finish();
@@ -95,7 +75,7 @@ public class SignIn extends AppCompatActivity {
 
             @Override
             public void onFailure(Call<ResponseModel> call, Throwable t) {
-                Toast.makeText(SignIn.this, "Error : " + t, Toast.LENGTH_SHORT).show();
+                Toast.makeText(SignIn.this, "Error : " +t ,Toast.LENGTH_SHORT).show();
                 finish();
             }
         });
@@ -109,7 +89,7 @@ public class SignIn extends AppCompatActivity {
     }
 
     // get data user by email
-    public void getByEmail(String email) {
+    public void getByEmail() {
         APIRequestData apiData = RetroServer.konekRetro().create(APIRequestData.class);
         Call<ResponseModel> userData = apiData.ardGetDataUserByEmail(email);
         userData.enqueue(new Callback<ResponseModel>() {
@@ -134,7 +114,7 @@ public class SignIn extends AppCompatActivity {
             @Override
             public void onFailure(Call<ResponseModel> call, Throwable t) {
                 reset();
-                Toast.makeText(SignIn.this, "Eror : " + t, Toast.LENGTH_SHORT).show();
+                Toast.makeText(SignIn.this, "Eror : "+t,  Toast.LENGTH_SHORT).show();
                 finish();
             }
         });
--- FriendFinderApp/app/src/main/java/com/example/friendfinderapp/Constants/ConfigurationAll.java
+++ FriendFinderApp/app/src/main/java/com/example/friendfinderapp/Constants/ConfigurationAll.java
@@ -3,7 +3,7 @@ package com.example.friendfinderapp.Constants;
 public class ConfigurationAll {
 
     public static String user_id = "";
-    static String ip = "192.168.2.19";
+    static String ip = "192.168.1.28";
     public static final String baseUrl = "http://" + ip + "/friend-finder/public/";
 
     public static String ImageURL = "http://" + ip + "/friend-finder/public/img/";
@@ -13,4 +13,3 @@ public class ConfigurationAll {
     public static final String THUMBNAIL_PLACE_URL = "http://" + ip + "/friend-finder/public/API/getDataPlace";
     public static final String PLACE_URL = "http://" + ip + "/friend-finder/public/API/getDataPlace";
 }
-
